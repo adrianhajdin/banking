6c7ec16cecbd60c48400019fcd93254e253c47d4
Gyanaranjan Sendha <75114005+grsendha@users.noreply.github.com>
2024-05-10 19:35:22 +0530

    Added loading and Suspense

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -2,29 +2,32 @@ import HeaderBox from '@/components/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
 import { getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
-import React from 'react'
+import React, { Suspense } from 'react'
+import Loading from '../loading';
 
 const Transfer = async () => {
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ 
-    userId: loggedIn.$id 
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
   })
 
-  if(!accounts) return;
-  
+  if (!accounts) return;
+
   const accountsData = accounts?.data;
 
   return (
-    <section className="payment-transfer">
-      <HeaderBox 
-        title="Payment Transfer"
-        subtext="Please provide any specific details or notes related to the payment transfer"
-      />
+    <Suspense fallback={<Loading />}>
+      <section className="payment-transfer">
+        <HeaderBox
+          title="Payment Transfer"
+          subtext="Please provide any specific details or notes related to the payment transfer"
+        />
 
-      <section className="size-full pt-5">
-        <PaymentTransferForm accounts={accountsData} />
+        <section className="size-full pt-5">
+          <PaymentTransferForm accounts={accountsData} />
+        </section>
       </section>
-    </section>
+    </Suspense>
   )
 }
 
